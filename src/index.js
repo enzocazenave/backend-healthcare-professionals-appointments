360fec42984f9b4ccf533fc8aa7e94231e56edab
Enzo Cazenave <enzocazenavedev@hotmail.com>
2025-04-24 23:50:32 -0300

    Change path of yaml swagger doc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import swaggerUi from 'swagger-ui-express';
-
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerPath = path.join(__dirname, 'swagger.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
 
 const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(YAML.load('./swagger.yaml'), { customSiteTitle: 'Healthcare Professionals Appointments API' }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customSiteTitle: 'Healthcare Professionals Appointments API'
+}));
 
 export default app;
