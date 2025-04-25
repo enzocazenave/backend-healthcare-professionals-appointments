e45e03ccc7532edf814f3447080cdfd9dd5ab93c
Enzo Cazenave <enzocazenavedev@hotmail.com>
2025-04-24 23:59:47 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerPath = path.join(__dirname, 'swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
 
 const app = express();
 
