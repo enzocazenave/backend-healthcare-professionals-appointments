99a84239ed1183027c48390416197e7579979f51
Enzo Cazenave <enzocazenavedev@hotmail.com>
2025-04-24 23:57:02 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const swaggerPath = path.join(__dirname, 'swagger.yaml');
-const swaggerDocument = YAML.load(swaggerPath);
+import swaggerDocument from './swagger.json' assert { type: 'json' };
 
 const app = express();
 
